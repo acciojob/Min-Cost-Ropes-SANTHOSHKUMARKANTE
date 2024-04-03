03f237531d760aeeb346870c2f802d1215fdb6c6
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-04-03 12:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 function mincost(arr)
 { 
 //write your code here
-	let arr2 = {};
+	// let arr2 = {};
+	let ans = 0;
 	arr.sort((a,b) => a-b);//{2,3,4,6}
-	let len = arr.length;//4
+	// let len = arr.length;//4
 	while(arr.length>1){
 		let sum = arr[0]+arr[1];//5 9 15
-		arr2.push(sum);//{5} {5,9} {5,9,15}
-		arr = arr.splice(2);//{5,4,6} {9,6} {15}
-		arr.push(sum);
-		arr.sort((a,b) => a-b);//{4,5,6} {6,9} {15}
+		// arr2.push(sum);//{5} {5,9} {5,9,15}
+		// arr = arr.splice(2);//{5,4,6} {9,6} {15}
+		// arr.push(sum);
+		// arr.sort((a,b) => a-b);//{4,5,6} {6,9} {15}
+		arr.shift();
+		arr.shift();
+		let i=0;
+		while (arr[i]<sum) {
+			i++;
+		}
+		if(i==0){
+			arr.unshift(sum);
+		}
+		else{
+			arr.splice(i,0,sum);
+		}
+		ans+=sum;
 	}
-	let res = 0;
-	for (let index = 0; index < arr2.length; index++) {
-		res+=arr2[index];
-	}
-	return res;
-	
-
-
-
-
+	return ans;
+	// let res = 0;
+	// for (let index = 0; index < arr2.length; index++) {
+	// 	res+=arr2[index];
+	// }
+	// return res;
 	
 // return the min cost
   
